fb10272743338430c6a8539e2af2976a9da27611
Josh Romney <jromney13@gmail.com>
2020-01-15 09:21:51 -0700

    Commit

--- python_strings.py
+++ python_strings.py
@@ -8,37 +8,42 @@
 #       -set this equal to your birth year (doesn't have to be real should be less then 100 yrs ago)
 #   - current_year
 #       -set this equal to 2020
-
-
-
+print()
+my_first_name = 'Josh'
+my_last_name = 'Romney'
+my_year_of_birth = 1993
+current_year = 2020
 
 # TODO String Indexing
 #   - Print the following items (one per line) (print using variables)
 #       - first name  
 #       - last name
 #       - first letter of your first name (use the +index)
-#       - second letter of your last name (use the -index)
+#       - last letter of your last name (use the -index)
 #       - first two letter of your first name (use the +index)
-#       - second two letter of your last name (use the -index)
+#       - last two letter of your last name (use the -index)
 
-
-
+print(my_first_name)
+print(my_last_name)
+print(my_first_name[0])
+print(my_last_name[-1])
+print(my_first_name[:2])
+print(my_last_name[-2:])
 
 #TODO Combining Strings
 #   - Print the following items (one per line) (print using variables)
 #       -first name and last name combined
 #       -first name six times
 
-
-
-
+print(my_first_name +  " " + my_last_name)
+print(my_first_name * 6)
 
 # TODO Formatting Strings
 #   - Print the following items (one per line) (print using variables)
 #       - first name last name -was born in- year of birth
 #       - first name last name -was born in- year of birth. first name -enjoyed celebrating- current year
 
-
+txt = my_first_name +
 
 # TODO Escape characters
 #   - Print the following items (one per line) (print using variables)
@@ -51,3 +56,5 @@
 #       - first name and last name in lower case
 #       - length of last name
 #       - first name and last name all in upper case
+
+print()
